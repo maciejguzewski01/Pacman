1033f742f7bbcd6141de516e54c5161939a087ef
MaciejGuzewski <>
2021-05-15 12:39:08 +0000

    Dalsza implementacja planszy

--- Board.cpp
+++ Board.cpp
@@ -1,12 +1,11 @@
 #include "Board.h"
 
 //konstruktor 
-Board::Board(Level_name level): level(level)
+Board::Board(Pacman & pacman_b, Level_name level): pacman_b(pacman_b),level(level)
 {
     generate_viruses();
     generate_walls();
-    
-    
+    generate_vaccines();
     
 }
 
@@ -73,4 +72,109 @@ void Board::generate_outside_walls()
   {
     fields[29][col].has_wall=true;
   }
+}
+
+
+
+//generuje szczepionki
+void Board::generate_vaccines()
+{
+  for(int row=1;row<30;row++)
+  {
+      for(int col=1;col<40;col++)
+      {
+          if(fields[row][col].has_wall==true ) continue;
+          if((row==1)and(col==1)) continue;
+          if((row=15)and(col>=16)and(col<=25)) continue;
+          
+          fields[row][col].has_vaccine=true;
+          total_number_of_vaccine++;
+      }
+  }
+
+
+}
+
+
+//zwraca całkowitą ilość szczepionek 
+int Board::get_total_vaccine_number()
+{
+    return total_number_of_vaccine;
+}
+
+
+//sprawdza czy pacman może wykonać dany ruch
+bool Board::can_pacman_move(Move_direction direction)
+{
+  sf::Vector2f plan;
+  plan=pacman_b.get_next_field_location(direction);
+  int row=plan.x;
+  int col=plan.y;
+
+  if((row<0)or(row>29)or(col<0)or(col>39)) exit(-1);
+  if(fields[row][col].has_wall) return false;
+
+  return true;
+}
+
+
+//sprawdza czy dany wirus może wykonać dany ruch
+bool Board::can_virus_move(int number,Move_direction direction)
+{
+  sf::Vector2f plan;
+  plan=viruses[number].get_next_field_location(direction);
+  int row=plan.x;
+  int col=plan.y;
+
+  if((row<0)or(row>29)or(col<0)or(col>39)) exit(-1);
+  if(fields[row][col].has_wall) return false;
+
+  return true;
+}
+
+
+//sprawdza czy na danym polu jest ściana
+bool Board::is_wall_on_field(int row,int col)
+{
+ if(fields[row][col].has_wall==true) return true;
+ return false;
+}
+
+//sprawdza czy na danym polu jest szczepionka
+bool Board::is_vaccine_on_field(int row,int col)
+{
+   if(fields[row][col].has_vaccine==true) return true;
+   return false;
+}
+
+//sprawdza czy na danym polu jest pacman
+bool Board::is_pacman_on_field(int row,int col)
+{
+    sf::Vector2f position;
+    position=pacman_b.get_position();
+    int x=position.x;
+    int y=position.y;
+    if((x==row) and(y==col)) return true;
+    return false;
+
+}
+
+//sprawdza czy na danym polu jest jakikolwiek wirus
+bool Board::is_any_virus_on_field(int row,int col)
+{
+ for(size_t i=0;i<viruses.size();i++)
+ {
+     sf::Vector2f position=viruses[i].get_position();
+     int x=position.x;
+     int y=position.y;
+     if((row==x)and(col==y)) return true;
+ }
+
+ return false;
+}
+
+//zwraca pozycję wirusa o podanym numerze
+sf::Vector2f Board::get_position_of_virus_number(int number)
+{
+   return viruses[number].get_position();
 }
--- Board.h
+++ Board.h
@@ -17,18 +17,32 @@ enum Level_name {FIRST,SECOND,THIRD};
 
 class Board
 {
+    Pacman & pacman_b;
     Field fields[30][40];
     Level_name level;
     std::vector<Coronavirus> viruses;
+    int total_number_of_vaccine=0;
+
     void generate_viruses();
     void generate_walls();
     void generate_outside_walls();
+    void generate_vaccines();
+    
     
     
  
 
     public:
-    Board(Level_name level);
+    Board(Pacman & pacman_b, Level_name level);
+    int get_total_vaccine_number();
+    bool can_pacman_move(Move_direction direction);
+    bool can_virus_move(int number,Move_direction direction);
+
+    bool is_wall_on_field(int row,int col);
+    bool is_vaccine_on_field(int row,int col);
+    bool is_pacman_on_field(int row,int col);
+    bool is_any_virus_on_field(int row,int col);
+    sf::Vector2f get_position_of_virus_number(int number);
 };
 
 
